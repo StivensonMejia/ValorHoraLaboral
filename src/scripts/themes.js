ff06003e56caf0417f7ab6014be3ba5f2f685669
Stivenson Mejia <st.domp@gmail.com>
2025-03-17 16:32:42 -0500

    First commit : deploy app. funcionalidad pendiete

---
+++ src/scripts/themes.js
@@ -0,0 +1,50 @@
+
+const buttonsColorContainer = document.querySelector(".secCreator_buttonsContainer");
+const checkboxTheme = document.querySelector(".secViewer_checkBox");
+const body = document.body;
+
+const savedColor = localStorage.getItem("selectedColor");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedColor) {
+    body.className = savedColor;
+}
+
+if (savedTheme) {
+    body.id = savedTheme;
+    checkboxTheme.checked = savedTheme === "dark_mode";
+}
+
+buttonsColorContainer.addEventListener("click", (event) => {
+    let selectionedColor = event.target.dataset.color
+    if (!selectionedColor) return;
+    
+    let newColor;
+    switch (selectionedColor) {
+        case "Green":
+            newColor = "theme_green";
+            break;
+        case "Blue":
+            newColor = "theme_blue";
+            break;
+        case "Pink":
+            newColor = "theme_pink";
+            break;
+        case "Red":
+            newColor = "theme_red";
+            break;
+        default:
+            newColor = "theme_green";
+            break;
+    }
+
+    body.className = newColor;
+    localStorage.setItem("selectedColor", newColor); // Guardar en localStorage
+    
+});
+
+checkboxTheme.addEventListener("change", (event) => {
+    const isChecked = event.target.checked
+    body.id = isChecked ? "dark_mode" : "light_mode";
+    localStorage.setItem("theme", body.id);
+});
